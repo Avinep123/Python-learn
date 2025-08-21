52dca2b67061b97aeb6478a480ac9a726e500e92
Avinep123 <avipachhai01@gmail.com>
2025-08-21 17:31:59 +0545

    function

---
+++ Functions/1_function_syntax.py
@@ -0,0 +1,5 @@
+def square_of_num(num):
+    square=num**2
+    print(square)
+
+square_of_num(4)
---
+++ Functions/3_function_multi.py
@@ -0,0 +1,6 @@
+def multiply(par1,par2):
+    return par1*par2
+
+print(multiply(1,5))
+print(multiply('a',5))
+print(multiply(2,'a'))
